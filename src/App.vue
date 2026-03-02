31ea1f2c992265565adebf40cf99ebf3857c1b8e
janavgowda <janav.gowda@tcu.edu>
2026-03-02 02:13:01 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
+
 <template>
-  <div>Hello, Vue!</div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+    <h1>v-model vs v-bind</h1>
+    <h2>message: {{ message }}</h2>
+    <div>
+      <p>v-model = v-bind + @input</p>
+      <input
+        type="text"
+        v-bind:value="message"
+        @input="message = $event.target.value"
+      />
+    </div>
+    <div>
+      <p>v-model</p>
+      <input type="text" v-model="message" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  let message = ref('Hello!')
+  </script>
+  
+  <style scoped></style>
+  
